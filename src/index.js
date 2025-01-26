33779ade2aa659fb51d70e33fa6226dc4985e68a
Uniquecon <akinwunmiolusegun277@gmail.com>
2025-01-27 00:26:46 +0100

    refactor: changed the files structure

--- src/index.js
+++ src/index.js
@@ -1,17 +1,13 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 import "./style.css";
 import Home from "./pages/home";
 import { PlayContextDiv } from "./pages/navigate";
 
-
-const Default = () => {
-
-  return (
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
     <PlayContextDiv>
-      <Home/>
+      <Home />
     </PlayContextDiv>
-  );
-};
-
-ReactDOM.render(<Default />, document.getElementById("root"));
+  </StrictMode>
+);
