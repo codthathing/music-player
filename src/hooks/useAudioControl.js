d6ac2217d01db87a1672f628ce9346d4a09cb4c8
Uniquecon <akinwunmiolusegun277@gmail.com>
2025-04-06 09:50:32 +0100

    fix: updated the play/pause button

--- src/hooks/useAudioControl.js
+++ src/hooks/useAudioControl.js
@@ -1,33 +1,38 @@
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import { NavigateContext } from "../contexts/NavigateContext";
 
 export const useAudioControl = () => {
-  const { audioStatus, setAudioStatus, audioCopy, setCurrentAudio, currentAudio: { current_ID }, audioPlayerRef, audioShuffle, setMusicTime, setAudioDuration } = useContext(NavigateContext);
+  const { audioStatus, setAudioStatus, audioCopy, setCurrentAudio, currentAudio: { current_ID }, audioPlayerRef, audioShuffle, setMusicTime } = useContext(NavigateContext);
+  const audioElement = audioPlayerRef.current;
 
-  const audioInitialAction = () => {
-    setAudioStatus((prevState) => ({ ...prevState, status: true }));
-    setTimeout(() => {
-      audioPlayerRef.current.play();
-      audioPlayerRef.current.addEventListener('timeupdate', () => setMusicTime(Math.ceil(audioPlayerRef.current.currentTime)));
-    }, 1);
-  };
+  useEffect(() => {
+    if (!audioElement) return;
+
+    audioElement.addEventListener("play", () => setAudioStatus((prevState) => ({ ...prevState, status: true })));
+    audioElement.addEventListener("pause", () => setAudioStatus((prevState) => ({ ...prevState, status: false })));
+    audioElement.addEventListener('timeupdate', () => setMusicTime(Math.ceil(audioElement.currentTime)));
+
+    return () => {
+      audioElement.removeEventListener("play", () => setAudioStatus((prevState) => ({ ...prevState, status: true })));
+      audioElement.removeEventListener("pause", () => setAudioStatus((prevState) => ({ ...prevState, status: false })));
+      audioElement.removeEventListener('timeupdate', () => setMusicTime(Math.ceil(audioElement.currentTime)));
+    }
+  }, []);
 
   const getAudio = (id, current_ID) => {
     if (audioStatus.no_audio) setAudioStatus((prevState) => ({ ...prevState, no_audio: false }));
     setCurrentAudio({ ...audioCopy.find((audio) => audio.id === id), current_ID: current_ID });
-    audioInitialAction();
+    setTimeout(() => audioElement.play(), 0);
   };
 
-  const updateAudioStatus = (new_status, action) => {
+  const updateAudioStatus = (action) => {
     if (audioCopy.length !== 0) {
       if (audioStatus.no_audio) {
         setAudioStatus((prevState) => ({ ...prevState, no_audio: false }));
         const startUpAudioId = audioShuffle ? Math.floor(Math.random() * audioCopy.length) : 0;
         setCurrentAudio({ ...audioCopy.find((audio, index) => index === startUpAudioId), current_ID: startUpAudioId });
-        setTimeout(() => audioPlayerRef.current.addEventListener('timeupdate', () => setMusicTime(Math.ceil(audioPlayerRef.current.currentTime))), 1)
       };
-      setAudioStatus((prevState) => ({ ...prevState, status: new_status }));
-      { action ? setTimeout(() => audioPlayerRef.current.pause(), 1) : setTimeout(() => audioPlayerRef.current.play(), 1) };
+      setTimeout(() => action ? audioElement.pause() : audioElement.play(), 0);
     };
   };
 
@@ -35,7 +40,7 @@ export const useAudioControl = () => {
     if (!audioStatus.no_audio) {
       const newAudioId = audioShuffle ? Math.floor(Math.random() * audioCopy.length) : type === "NEXT" ? (current_ID < audioCopy.length - 1 ? current_ID + 1 : 0) : current_ID > 0 ? current_ID - 1 : audioCopy.length - 1;
       setCurrentAudio({ ...audioCopy.find((audio, index) => index === newAudioId), current_ID: newAudioId });
-      audioInitialAction();
+      setTimeout(() => audioElement.play(), 0);
     };
   };
 
